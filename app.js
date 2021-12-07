54411ea817ad03fc978ccd58a3db81f85d6e5d17
BrendenSeifried <brendenseifried@gmail.com>
2021-12-07 15:37:20 -0800

    working

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const nameInput = document.getElementById('name-input'); 
 
 const animalName = document.getElementById('animal-display');
-console.log(animalName);
+//console.log(animalName);
 
 nameInput.addEventListener('input', () => {
   // console.log('input changed', nameInput.value); 
    animalName.textContent = nameInput.value;
+});
+/*----------------------------------------------------------- */
+
+const sender = document.getElementById('sender'); 
+
+const sent = document.getElementById('sent');
+//console.log(showName);
+
+sender.addEventListener('input', () => {
+  // console.log('input changed', nameInput.value); 
+  sent.textContent = sender.value;
+});
+   
+//------------------------------------------------------------------------------------
+
+
+  const themeSelect = document.getElementById('theme-select');
+themeSelect.addEventListener('change', () => {
+  //character.classList.value = '';
+  //character.classList.add(themeSelect.value);
 });
